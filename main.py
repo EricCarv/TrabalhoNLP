762eb8077aa2c817ceb27e0f80bdee01a291c9f4
ericsson <ericsson_eduardo@hotmail.com>
2026-03-11 09:33:37 -0300

    removidos stops do debug

--- main.py
+++ main.py
@@ -19,20 +19,16 @@ df = pd.read_csv('pre-processed.csv')
 def limpeza_dataframe(texto):
     if not isinstance(texto, str) or texto == "":
         return ""
-    print(f"Original: {texto[:50]}...")
     texto = texto.lower()
     texto = unicodedata.normalize('NFKD', texto).encode('ascii', 'ignore').decode('utf-8')
     texto = re.sub(r'\d+', '',texto)
     texto = re.sub(r'[^\w\s]', '',texto)
     tokens = nltk.word_tokenize(texto)
-    print(f"Após Tokenizar: {tokens[:5]}")
     paradas = set(stopwords.words('portuguese'))
     tokens = [t for t in tokens if t not in paradas]
-    print(f"Após Stopwords: {tokens[:5]}")
     stemmer = RSLPStemmer()
     tokens = [stemmer.stem(t) for t in tokens]
     resultado = " ".join(tokens)
-    print(f"Resultado Final: {resultado[:50]}")
     return resultado
 
 df['preprocessed_news'] = df['preprocessed_news'].apply(limpeza_dataframe)
